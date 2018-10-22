a31db1ba3b58ecf43d958891e2c95043dd4fd67e
VinceX2 <vince.geek404@gmail.com>
2018-10-22 13:16:23 -0700

    Test1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'firstTest';
+  users: string[] = ['Joe', 'Viente', 'Galilea', 'Antonio'];
+  activated = true;
+  name = 'Vicente';
+  age: number;
+  address: {
+    street: string;
+    city: string;
+  };
+
+  hobbies: string[];
+
+  constructor() {
+    this.age = 28;
+    this.address = {
+      street: '221B Baker Street',
+      city: 'London'
+    };
+    this.hobbies = ['swimming', 'read', 'write'];
+  }
+
+  deleteUser(user: string) {
+    for (let i = 0; i < this.users.length; i++) {
+      if (user === this.users[i]) {
+        this.users.splice(i, 1);
+      }
+    }
+  }
+
+  addUser(newUser) {
+    this.users.push(newUser.value);
+    newUser.value = '';
+    newUser.focus();
+    return false;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { UserComponent } from './user/user.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule,
